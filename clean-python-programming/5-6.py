680e27dc8a4a687d58ff2e88ad43572e935dae22
kimkiyong <fenderst0612@gmail.com>
2024-01-14 18:56:36 +0900

    feat: class

---
+++ clean-python-programming/5-6.py
@@ -0,0 +1,11 @@
+import random
+class Dice:
+    def __init__(self, sides=6):
+        self.sides = sides
+    def roll(self):
+        return random.randint(1, self.sides)
+    
+d = Dice()
+print('You rolled a', d.roll())
+# > python clean-python-programming/5-6.py
+# You rolled a 2
